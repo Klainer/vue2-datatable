f3d9608cdbc585de40327e861a0d15b5f0ea4908
Klainer <martinhascak@gmail.com>
2018-07-17 12:51:09 +0200

    IE11 support

--- build/examples.js
+++ build/examples.js
@@ -2,6 +2,7 @@
 var path = require('path')
 var webpack = require('webpack')
 var CleanWebpackPlugin = require('clean-webpack-plugin')
+var BabelEnginePlugin = require("babel-engine-plugin")
 var dist = path.join(__dirname, '../examples/dist')
 
 module.exports = {
@@ -18,7 +19,10 @@ module.exports = {
     plugins: (function () {
       var plugins = [
         // https://webpack.js.org/plugins/ignore-plugin/
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new BabelEnginePlugin({
+          presets: ['env']
+        })
       ]
       if (process.env.NODE_ENV === 'production') {
         plugins.push(
