a92c69a8ede9fe8a2febd8c9c09ef5838c52f3e4
Martin Haščák <klainer@seznam.cz>
2018-09-04 08:37:59 +0200

    Update Main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Datatable from './index.vue'
 
-Datatable.install = function(Vue, options)  {
-  const locale = options ? options.locale : {};
-  
+Datatable.install = (Vue, options) => {
+  const locale = options ? options.locale : {}
+
   // this might be the simplest i18n solution
-  Vue.prototype.$i18nForDatatable = function (srcTxt) { return locale[srcTxt] || srcTxt; };
-  Vue.component('Datatable', Datatable);
+  Vue.prototype.$i18nForDatatable = srcTxt => locale[srcTxt] || srcTxt
+
+  Vue.component('Datatable', Datatable)
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(Datatable);
+  window.Vue.use(Datatable)
 }
 
-export default Datatable;
+export default Datatable
